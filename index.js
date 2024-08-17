d73fb3c9cb9908494a75c03da3d94f3d5c2680d7
wardhan007 <118925155+wardhan007@users.noreply.github.com>
2024-08-18 00:19:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+const dotenv= require("dotenv")
+const mongoose = require("mongoose")
+const authRoute=require("./routes/auth")
+
+dotenv.config()
+app.use(express.json())
+
+mongoose
+.connect(process.env.CONNECTION_URL,{
+    useNewUrlParser : true,
+    useUnifiedTopology: true,
+})
+.then(console.log("Connected to MongoDB"))
+.catch((err)=> console.log(err))
+
+app.use("/auth",authRoute)
+
+app.listen("5000",() =>{
+    console.log("backend running")
+})
---
+++ user.js
@@ -0,0 +1,9 @@
+const router = require("express").Router()
+const User = require("../model/User")
+const bcrypt =require("bcrypt")
+
+//update
+router.put("/:id",async (req,res)=> {
+    if(req.body.userId== )
+} )
+module.exports = router
